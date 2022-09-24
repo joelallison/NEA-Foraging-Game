339622e6e5772332de8523cf42f6c2ff240ba667
Joel <33546240+joelallison@users.noreply.github.com>
2022-09-24 18:49:10 +0100

    Working on email system

--- core/src/com/joelallison/users/Email.java
+++ core/src/com/joelallison/users/Email.java
@@ -1,13 +1,26 @@
 package com.joelallison.users;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Date;
-
 import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class Email {
 
-    public static void sendEmail(){
+    public static void sendEmail(Session session, String toEmail, String subject, String body){
+        try {
+            MimeMessage msg = new MimeMessage(session);
 
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+            msg.addHeader("format", "flowed");
+            msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+            msg.setFrom(new InternetAddress(""));
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+
 }
