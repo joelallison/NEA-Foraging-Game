72b4295899817246a905f5e53c676ee4be641397
Joel <33546240+joelallison@users.noreply.github.com>
2022-09-24 13:07:28 +0100

    Working on email system

---
+++ core/src/com/joelallison/users/Email.java
@@ -0,0 +1,13 @@
+package com.joelallison.users;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+
+import javax.mail.*;
+
+public class Email {
+
+    public static void sendEmail(){
+
+    }
+}
