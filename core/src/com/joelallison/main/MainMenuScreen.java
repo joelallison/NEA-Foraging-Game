045a4f19c8e5f8b245b76b5c5bce9c66e57516e2
Joel <33546240+joelallison@users.noreply.github.com>
2022-09-24 19:31:33 +0100

    Implementing menu UI

--- core/src/com/joelallison/main/MainMenuScreen.java
+++ core/src/com/joelallison/main/MainMenuScreen.java
@@ -1,15 +1,45 @@
 package com.joelallison.main;
 
 import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.joelallison.level.TileType;
+import com.badlogic.gdx.scenes.scene2d.*;
+
 
 public class MainMenuScreen implements Screen {
 
     final Init system;
+    private Stage stage;
+    private Table table;
 
     public MainMenuScreen(final Init system) {
         this.system = system;
+
+        stage = new Stage();
+        //stage.setViewport(system.viewport);
+        Gdx.input.setInputProcessor(stage);
+
+        table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        table.setDebug(true);
+
+        /*TextureRegion upRegion =
+        TextureRegion downRegion =
+        BitmapFont buttonFont = ...
+
+        TextButton.TextButtonStyle style = new TextButton.TextButtonStyle();
+        style.up = new TextureRegionDrawable(upRegion);
+        style.down = new TextureRegionDrawable(downRegion);
+        style.font = buttonFont;
+
+        TextButton button1 = new TextButton("Button 1", style);
+        table.add(button1);*/
     }
 
     @Override
@@ -42,17 +72,17 @@ public class MainMenuScreen implements Screen {
     public void render(float delta) {
         ScreenUtils.clear(0.1215686f, 0.09411765f, 0.07843137f, 1);
 
-        system.batch.begin();
-        system.font.draw(system.batch, "HELLO", 100, 150);
-        system.font.draw(system.batch, "[press space]", 100, 100);
-        system.batch.end();
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
     public void dispose () {
+        stage.dispose();
     }
 }
