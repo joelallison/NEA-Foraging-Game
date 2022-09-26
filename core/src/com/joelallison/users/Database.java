d81eb0f5b17c4705ce39fd127c3fc272044a8965
Joel <33546240+joelallison@users.noreply.github.com>
2022-09-26 13:30:37 +0100

    Beginning implementing database system

---
+++ core/src/com/joelallison/users/Database.java
@@ -0,0 +1,36 @@
+package com.joelallison.users;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Database {
+
+    public static void main(String[] args){
+        String jdbcURL = "jdbc:postgresql://localhost:5432/levelgentool";
+        String username = "postgres";
+        String password = "password";
+
+        try {
+            Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+            System.out.println("Connected to PostgreSQL server");
+
+            String sql = "INSERT INTO users(username, hashed_password, email)"+
+                    "VALUES ('joelallison', 'asjdlansdasd', 'thatjoelallison@gmail.com')";
+
+            Statement statement = connection.createStatement();
+
+            int rows = statement.executeUpdate(sql);
+            if (rows > 0){
+                System.out.println("New user added.");
+            }
+
+            connection.close();
+
+        } catch (SQLException e){
+            System.out.println("PostgreSQL Server Connection error");
+            e.printStackTrace();
+        }
+    }
+}
