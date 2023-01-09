de4bf7f752be4413f3e9e90d6eb5a67c296e861b
joelallison <thatjoelallison@gmail.com>
2023-01-09 13:07:05 +0000

    insert Song 2 by Blur here

--- core/src/com/joelallison/screens/UserInterface/UserInterface.java
+++ core/src/com/joelallison/screens/UserInterface/UserInterface.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import sun.plugin.javascript.navig4.Layer;
 
 import java.util.HashMap;
 
@@ -84,7 +85,9 @@ public class UserInterface {
         }
     }
 
-    protected class Test extends WidgetGroup {
+    protected HorizontalGroup createLayer(Layer layer) {
 
+
+        return null;
     }
 }
