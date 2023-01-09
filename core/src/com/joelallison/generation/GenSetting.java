ad189c5969df99229d69c56aeeaabc82a8c89972
joelallison <thatjoelallison@gmail.com>
2023-01-09 12:12:42 +0000

    yeahh h hh

--- core/src/com/joelallison/generation/GenSetting.java
+++ core/src/com/joelallison/generation/GenSetting.java
@@ -1,10 +1,26 @@
 package com.joelallison.generation;
 
+import com.joelallison.display.Tileset;
 import com.joelallison.display.Tileset.*;
 
 abstract class GenSetting {
     protected String name;
     protected Long seed;
+    protected Tileset tileSet;
+    protected boolean showLayer;
+    protected boolean opened;
+
+
+    // when you move the layer, a layer gets stored separately, another gets copied over to where it will move,
+    // and then the separate one gets stored in its final place, writing over the recently moved one's old duplicate
+
+    // layer has name, settings, chosen tileset and tile children that function in a way specific to the gen type
+    // the layer itself can be moved up or down
+    // layer settings edited on the left, upon selecting the layer
+    // children boundaries etc. are edited on the right as part of the layer box.
+    // export layer button
+    // clipping mode?
+    // the spacing and wave collapse feature?
 
     public GenSetting(String name, Long seed) {
         this.name = name;
